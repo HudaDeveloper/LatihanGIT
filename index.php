baa2471b32662bfa4a9c383236590b8b7774eccd
Huda <hudasaputra046@gmail.com>
2025-11-10 11:13:22 +0700

    update v1

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php 
-echo "LatihanGit";
+<?php
+class Manusia{
+
+   public $nama;
+   public $umur;
+
+   public function biodatamanusia() {
+       echo "Halo Selamat Datang $this->nama";
+       echo "Umur Kamu Adalah : $this->umur";
+   } 
+
+   public function isibiodatamanusia() {
+       $this->nama = readline("Masukkan Nama Mu : ");
+       $this->umur = readline("Masukkan Umur Mu : ");
+   }
+
+   public function __destruct() {
+       echo "Objek Manusia dengan nama $this->nama dan $this->umur telah di destroy.\n";
+   }
+}
 ?>
---
+++ view.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'index.php';
+
+//object
+$manusia = new Manusia();
+
+//function
+$manusia->isibiodatamanusia();
+$manusia->biodatamanusia();
+?>
